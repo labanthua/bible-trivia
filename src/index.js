583dec0a25bf6726f1dc86a4484eceaf43be05c6
labanthua <52541707+labanthua@users.noreply.github.com>
2019-07-26 15:56:08 +0300

    Commiting to Master

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,00.00');
+const courseValue = numeral(100).format('$0,00.00');
+        chapter = 'genesis';
 debugger; // eslint-disable-line no-debugger
-console.log(`I would Pay ${courseValue} for this Awesome Bible Trivia`); // eslint-disable-line no-console
+console.log(`I would Pay ${courseValue} for this Awesome Bible Trivia!!!!, chapter`); // eslint-disable-line no-console
+
