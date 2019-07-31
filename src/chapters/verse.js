160a243dbb073b29282ef4b84324301d32a8f655
labanthua <52541707+labanthua@users.noreply.github.com>
2019-07-31 17:40:27 +0300

    Commiting the Chapters folder

---
+++ src/chapters/verse.js
@@ -0,0 +1,5 @@
+export class Verse {
+    constructor(id) {
+    this.id = id;
+    }         
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import './index.css';
 import numeral from 'numeral';
+import { Verse } from './chapters/verse.js';
 
 const courseValue = numeral(100).format('$0,00.00');
-        chapter = 'genesis';
+        
 debugger; // eslint-disable-line no-debugger
 console.log(`I would Pay ${courseValue} for this Awesome Bible Trivia!!!!, chapter`); // eslint-disable-line no-console
+ 
+let verse = new Verse(1);
+console.log(verse.id); // eslint-disable-line no-console
+console.log(location.href);// eslint-disable-line no-console
+        
+
+
+
 
